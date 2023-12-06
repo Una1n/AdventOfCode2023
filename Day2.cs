eabcc744140a50ed64abc071f1b44fe9145639c5
Unain <vanstoel@gmail.com>
2023-12-06 13:58:50 +0100

    Added Day 2 Puzzle 1 Answer

--- Day2.cs
+++ Day2.cs
@@ -1,15 +1,50 @@
-﻿namespace AdventOfCode2023
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
 {
 	internal class Day2
 	{
+		static readonly Dictionary<string, int> maxCubes = new(){
+			{"red", 12},
+			{"green", 13},
+			{"blue", 14}
+		};
+
 		public static void RunPuzzle1()
 		{
 			string[] lines = File.ReadAllLines(@"Day2-Input.txt");
+			int sumIDs = 0;
 			foreach (string line in lines)
 			{
+				Match matches = Regex.Match(line, "^Game ([0-9]+): (.*)");
+				int gameID = int.Parse(matches.Groups[1].Value);
+				string gameCubes = matches.Groups[2].Value;
+				bool gamePossible = true;
+
+				string[] sets = gameCubes.Split(';');
+				foreach (string set in sets)
+				{
+					string[] cubeSets = set.Split(',');
+					foreach (string cubeSet in cubeSets)
+					{
+						string cubeSetTrimmed = cubeSet.Trim();
+						string[] cube = cubeSetTrimmed.Split(' ');
+
+						int cubeAmount = int.Parse(cube[0]);
+						if (cubeAmount > maxCubes[cube[1]])
+						{
+							gamePossible = false;
+						}
+					}
+				}
+
+				if (gamePossible)
+				{
+					sumIDs += gameID;
+				}
 			}
 
-			//Console.WriteLine("Answer Puzzle 1: " + sumCalibration);
+			Console.WriteLine("Answer Puzzle 1: " + sumIDs);
 		}
 
 		public static void RunPuzzle2()
