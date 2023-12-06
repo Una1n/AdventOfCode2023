3737e391bb945d8c690de96dd5b3095580f2d10e
Unain <vanstoel@gmail.com>
2023-12-06 19:05:19 +0100

    Added Day 3 Puzzle 1 Answer

---
+++ Day3.cs
@@ -0,0 +1,90 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
+{
+	internal class Day3
+	{
+		public class EnginePartNumber
+		{
+			public List<Vector2> positions = new();
+			public int partNr;
+
+			public EnginePartNumber(int nr, Vector2 firstPosition)
+			{
+				partNr = nr;
+				positions.Add(firstPosition);
+
+				if (partNr.ToString().Length > 1)
+				{
+					for (int i = 1; i < partNr.ToString().Length; i++)
+					{
+						positions.Add(new Vector2(firstPosition.X + i, firstPosition.Y));
+					}
+				}
+			}
+
+			public bool IsAdjacentToSymbol(Vector2 symbolPosition)
+			{
+				foreach (Vector2 position in positions)
+				{
+					float distance = Vector2.DistanceSquared(symbolPosition, position);
+					if (distance <= 2)
+					{
+						return true;
+					}
+				}
+
+				return false;
+			}
+		}
+
+		public static void RunPuzzle1()
+		{
+			string[] lines = File.ReadAllLines(@"Day3-Input.txt");
+			int numberOfCharacters = lines.Sum(s => s.Length);
+			int sumEngineParts = 0;
+			int x = 0, y = 0;
+			List<EnginePartNumber> parts = new();
+			List<Vector2> symbolLocations = new();
+			foreach (string line in lines)
+			{
+				Regex regex = new Regex(@"\d+");
+				foreach (Match match in regex.Matches(line))
+				{
+					parts.Add(new(int.Parse(match.Value), new Vector2(match.Index, y)));
+				}
+
+				foreach (char c in line.ToCharArray())
+				{
+					if (!char.IsNumber(c) && c != '.')
+					{
+						symbolLocations.Add(new Vector2(x, y));
+					}
+
+					x++;
+				}
+
+				x = 0;
+				y++;
+			}
+
+			foreach (EnginePartNumber part in parts)
+			{
+				foreach (Vector2 symbolPosition in symbolLocations)
+				{
+					if (part.IsAdjacentToSymbol(symbolPosition))
+					{
+						sumEngineParts += part.partNr;
+					}
+				}
+			}
+
+			Console.WriteLine("Answer Puzzle 1: " + sumEngineParts);
+		}
+
+		public static void RunPuzzle2()
+		{
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ Console.Clear();
 Console.WriteLine("Choose Advent Day to run:");
 Console.WriteLine("1) Day 1");
 Console.WriteLine("2) Day 2");
+Console.WriteLine("3) Day 3");
 Console.Write("\r\nSelect an option: ");
 
 switch (Console.ReadLine())
@@ -15,5 +16,9 @@ switch (Console.ReadLine())
 	case "2":
 		Day2.RunPuzzle1();
 		Day2.RunPuzzle2();
+		break;
+	case "3":
+		Day3.RunPuzzle1();
+		Day3.RunPuzzle2();
 		break;
 }
