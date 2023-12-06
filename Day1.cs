b051275383702dafe9c16d1c17764b639edd9339
Unain <vanstoel@gmail.com>
2023-12-06 13:07:03 +0100

    Added Day 1 Puzzle 2

--- Day1.cs
+++ Day1.cs
@@ -1,7 +1,21 @@
-﻿namespace AdventOfCode2023
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
 {
 	internal class Day1
 	{
+		static readonly Dictionary<string, string> wordNumbers = new(){
+			{"one", "1"},
+			{"two", "2"},
+			{"three", "3"},
+			{"four", "4"},
+			{"five", "5"},
+			{"six", "6"},
+			{"seven", "7"},
+			{"eight", "8"},
+			{"nine", "9"}
+		};
+
 		public Day1()
 		{
 			//
@@ -13,7 +27,7 @@
 			int sumCalibration = 0;
 			foreach (string line in lines)
 			{
-				string twoDigitString = line.ToCharArray().First(Char.IsNumber).ToString() + line.ToCharArray().Last(Char.IsNumber);
+				string twoDigitString = line.ToCharArray().First(char.IsNumber).ToString() + line.ToCharArray().Last(char.IsNumber);
 				sumCalibration += int.Parse(twoDigitString);
 			}
 
@@ -22,15 +36,39 @@
 		
 		public static void RunPuzzle2()
 		{
-			//string[] lines = File.ReadAllLines(@"Day1-1.txt");
-			//int sumCalibration = 0;
-			//foreach (string line in lines)
-			//{
-			//	string twoDigitString = line.ToCharArray().First(Char.IsNumber).ToString() + line.ToCharArray().Last(Char.IsNumber);
-			//	sumCalibration += int.Parse(twoDigitString);
-			//}
+			string[] lines = File.ReadAllLines(@"Day1-1.txt");
+			int sumCalibration = 0;
+			string digitOne, digitTwo = "";
+			foreach (string line in lines)
+			{
+				Match matches = Regex.Match(line, "(one|two|three|four|five|six|seven|eight|nine|\\d)");
+				digitOne = matches.Value;
 
-			//Console.WriteLine("Answer Puzzle 2: " + sumCalibration);
+				for (int i = 1; i <= line.Length; i++)
+				{
+					string endString = line[^i..];
+					matches = Regex.Match(endString, "(one|two|three|four|five|six|seven|eight|nine|\\d)");
+					if (!string.IsNullOrEmpty(matches.Value))
+					{
+						digitTwo = matches.Value;
+						break;
+					}
+				}
+
+				if (!int.TryParse(digitOne, out int firstDigit))
+				{
+					digitOne = wordNumbers[digitOne];
+				}
+
+				if (!int.TryParse(digitTwo, out int secondDigit))
+				{
+					digitTwo = wordNumbers[digitTwo];
+				}
+
+				sumCalibration += int.Parse(digitOne + digitTwo);
+			}
+
+			Console.WriteLine("Answer Puzzle 2: " + sumCalibration);
 		}
 	}
 }
