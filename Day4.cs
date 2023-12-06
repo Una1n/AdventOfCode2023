3fc4d8b90cf163feff88cd27dc2a7f189bff5696
Unain <vanstoel@gmail.com>
2023-12-06 19:57:48 +0100

    Added Initial Day 4

---
+++ Day4.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2023
+{
+	internal class Day4
+	{
+		public static void RunPuzzle1()
+		{
+
+		}
+
+		public static void RunPuzzle2()
+		{
+
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using AdventOfCode2023;
 
 Console.Clear();
-Console.WriteLine("Choose Advent Day to run:");
+Console.WriteLine("**** Advent of Code 2023 ****");
+Console.WriteLine("Choose Day to run:");
 Console.WriteLine("1) Day 1");
 Console.WriteLine("2) Day 2");
 Console.WriteLine("3) Day 3");
+Console.WriteLine("4) Day 4");
 Console.Write("\r\nSelect an option: ");
 
 switch (Console.ReadLine())
@@ -20,5 +22,9 @@ switch (Console.ReadLine())
 	case "3":
 		Day3.RunPuzzle1();
 		Day3.RunPuzzle2();
+		break;
+	case "4":
+		Day4.RunPuzzle1();
+		Day4.RunPuzzle2();
 		break;
 }
