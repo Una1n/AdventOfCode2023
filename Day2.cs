ef9da861eaf9844822368dc37834246dabd27342
Unain <vanstoel@gmail.com>
2023-12-06 14:19:28 +0100

    Added Day 2 Puzzle 2 Answer

--- Day2.cs
+++ Day2.cs
@@ -18,10 +18,10 @@ namespace AdventOfCode2023
 			{
 				Match matches = Regex.Match(line, "^Game ([0-9]+): (.*)");
 				int gameID = int.Parse(matches.Groups[1].Value);
-				string gameCubes = matches.Groups[2].Value;
+				string gamesLine = matches.Groups[2].Value;
 				bool gamePossible = true;
 
-				string[] sets = gameCubes.Split(';');
+				string[] sets = gamesLine.Split(';');
 				foreach (string set in sets)
 				{
 					string[] cubeSets = set.Split(',');
@@ -50,12 +50,44 @@ namespace AdventOfCode2023
 		public static void RunPuzzle2()
 		{
 			string[] lines = File.ReadAllLines(@"Day2-Input.txt");
+			int sumPowers = 0;
 			foreach (string line in lines)
 			{
-				//
+				Match matches = Regex.Match(line, "^Game ([0-9]+): (.*)");
+				string gamesLine = matches.Groups[2].Value;
+				int maxRed = 0;
+				int maxBlue = 0;
+				int maxGreen = 0;
+
+				string[] sets = gamesLine.Split(';');
+				foreach (string set in sets)
+				{
+					string[] cubeSets = set.Split(',');
+					foreach (string cubeSet in cubeSets)
+					{
+						string cubeSetTrimmed = cubeSet.Trim();
+						string[] cube = cubeSetTrimmed.Split(' ');
+
+						int cubeAmount = int.Parse(cube[0]);
+						if (cube[1] == "red" && cubeAmount > maxRed)
+						{
+							maxRed = cubeAmount;
+						}
+						else if (cube[1] == "blue" && cubeAmount > maxBlue)
+						{
+							maxBlue = cubeAmount;
+						}
+						else if (cube[1] == "green" && cubeAmount > maxGreen)
+						{
+							maxGreen = cubeAmount;
+						}
+					}
+				}
+
+				sumPowers += (maxRed * maxBlue * maxGreen);
 			}
 
-			//Console.WriteLine("Answer Puzzle 2: " + sumCalibration);
+			Console.WriteLine("Answer Puzzle 2: " + sumPowers);
 		}
 	}
 }
