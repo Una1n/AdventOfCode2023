e4a326f4520e52e077212a39cef96addc02f2cb0
Unain <vanstoel@gmail.com>
2023-12-06 10:30:29 +0100

    Added Console Menu + initial Day1

---
+++ Day1.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2023
+{
+	internal class Day1
+	{
+		public Day1()
+		{
+			Console.WriteLine("Running Day 1");
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AdventOfCode2023;
+
+Console.Clear();
+Console.WriteLine("Choose Advent Day to run:");
+Console.WriteLine("1) Day 1");
+Console.WriteLine("2) Exit");
+Console.Write("\r\nSelect an option: ");
+
+switch (Console.ReadLine())
+{
+	case "1":
+		Day1 day1 = new();
+		break;
+}
