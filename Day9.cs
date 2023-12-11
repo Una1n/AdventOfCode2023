2efe0c561c50b7d1e8aabbfb9307b3ee43ab45db
Unain <vanstoel@gmail.com>
2023-12-11 15:04:41 +0100

    Added Day 9 Puzzle 2 Answer

--- Day9.cs
+++ Day9.cs
@@ -54,9 +54,48 @@ namespace AdventOfCode2023
 		public static void RunPuzzle2()
 		{
 			string[] lines = File.ReadAllLines(@"Day9-Input.txt");
+			int sumHistory = 0;
+			foreach (string line in lines)
+			{
+				Regex regex = new Regex(@"[\d-]+");
+				List<int> list = new();
+				foreach (Match match in regex.Matches(line))
+				{
+					list.Add(int.Parse(match.Value));
+				}
 
+				List<int> firstNumbers = new();
+				firstNumbers.Add(list[0]);
+				bool foundEnd = false;
+				List<int> listToCheck = list;
+				while (!foundEnd)
+				{
+					List<int> nextNumbers = new();
+					firstNumbers.Add(listToCheck[1] - listToCheck[0]);
+					for (int i = 1; i < listToCheck.Count; i++)
+					{
+						int answer = listToCheck[i] - listToCheck[i - 1];
+						nextNumbers.Add(answer);
+					}
 
-			//Console.WriteLine("Answer Puzzle 2: " + steps);
+					if (nextNumbers.Distinct().Count() == 1)
+					{
+						foundEnd = true;
+					}
+
+					listToCheck = nextNumbers;
+				}
+
+				int newNumber = firstNumbers[^1];
+				for (int x = firstNumbers.Count - 1; x > 0; x--)
+				{
+					newNumber = firstNumbers[x - 1] - newNumber;
+				}
+
+				sumHistory += newNumber;
+			}
+
+			Console.WriteLine("Answer Puzzle 2: " + sumHistory);
 		}
 	}
 }
