e7772a540b5dfb886171427daa17465babd92c83
Unain <vanstoel@gmail.com>
2023-12-11 15:30:08 +0100

    Added Initial Day 10

---
+++ Day10.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2023
+{
+	internal class Day10
+	{
+		public static void RunPuzzle1()
+		{
+			string[] lines = File.ReadAllLines(@"Day10-Input.txt");
+			char[,] map = new char[lines.Length, lines[0].Length];
+
+			foreach (string line in lines)
+			{
+				//
+			}
+			//Console.WriteLine("Answer Puzzle 1: " + sumHistory);
+		}
+
+		public static void RunPuzzle2()
+		{
+			string[] lines = File.ReadAllLines(@"Day10-Input.txt");
+
+
+			//Console.WriteLine("Answer Puzzle 2: " + sumHistory);
+		}
+	}
+}
