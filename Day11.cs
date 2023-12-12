b401af1fe4e6c7cfd3ca857bf9e68da362178923
Unain <vanstoel@gmail.com>
2023-12-12 18:08:39 +0100

    Refactored Day 11 Puzzle 1 Answer

--- Day11.cs
+++ Day11.cs
@@ -20,13 +20,11 @@ namespace AdventOfCode2023
 		{
 			string[] lines = File.ReadAllLines(@"Day11-Input.txt");
 
-			int lineLength = lines[0].Length;
-			string[] expandedUniverse = lines;
-			List<int> lineIndexes = new();
-			for (int y = 0; y < lineLength; y++)
+			// Get Expansion X Positions
+			List<int> expansionXPositions = new();
+			for (int y = 0; y < lines[0].Length; y++)
 			{
-				int lineIndex = y;
-				if (lines[0][lineIndex] == '.')
+				if (lines[0][y] == '.')
 				{
 					bool foundGalaxy = false;
 					for (int x = 0; x < lines.Length; x++)
@@ -39,36 +37,28 @@ namespace AdventOfCode2023
 
 					if (!foundGalaxy)
 					{
-						lineIndexes.Add(lineIndex);
+						expansionXPositions.Add(y);
 					}
 				}
 			}
 
-			int offset = 0;
-			foreach (int index in lineIndexes)
+			// Get Expansion Y Positions
+			int indexY = 0;
+			List<int> expansionYPositions = new();
+			foreach (string line in lines)
 			{
-				for (int i = 0; i < lines.Length; i++)
-				{
-					expandedUniverse[i] = lines[i].Insert(index + offset, ".");
-				}
-				offset++;
-			}
-
-			List<string> map = new();
-			foreach (string line in expandedUniverse)
-			{
-				map.Add(line);
-
 				if (!line.Contains('#'))
 				{
-					map.Add(line);
+					expansionYPositions.Add(indexY);
 				}
+				indexY++;
 			}
 
+			// Get all galaxies
 			int yPos = 0;
 			int galaxyID = 1;
 			List<Galaxy> galaxies = new();
-			foreach (string m in map)
+			foreach (string m in lines)
 			{
 				if (m.Contains('#'))
 				{
@@ -88,12 +78,50 @@ namespace AdventOfCode2023
 			int sumDistancesGalaxy = 0;
 			foreach (Galaxy g in galaxies)
 			{
+				//Console.WriteLine("Galaxy: {0}, position: {1}", g.ID, g.position);
 				for (int x = galaxyIndex; x < galaxies.Count; x++)
 				{
+					int extraSteps = 0;
 					Galaxy nextGalaxy = galaxies[x];
-					int stepsX = int.Abs((int)nextGalaxy.position.X - (int)g.position.X);
-					int stepsY = int.Abs((int)nextGalaxy.position.Y - (int)g.position.Y);
-					sumDistancesGalaxy += stepsY + stepsX;
+					int stepsX = (int)nextGalaxy.position.X - (int)g.position.X;
+					int stepsY = (int)nextGalaxy.position.Y - (int)g.position.Y;
+					int stepsWithoutExpansion = int.Abs(stepsX) + int.Abs(stepsY);
+					foreach (int expandXPosition in expansionXPositions)
+					{
+						Vector2 expandPos = new(expandXPosition, g.position.Y);
+
+						int endPosX = (int)nextGalaxy.position.X - (int)g.position.X;
+						int expansionPosX = (int)nextGalaxy.position.X - (int)expandPos.X;
+						if (endPosX < 0)
+						{
+							if (expansionPosX > endPosX && expansionPosX < 0)
+							{
+								extraSteps++;
+							}
+						}
+						else
+						{
+							if (expansionPosX > 0 && expansionPosX < endPosX)
+							{
+								extraSteps++;
+							}
+						}
+					}
+
+					foreach (int expandYPosition in expansionYPositions)
+					{
+						Vector2 expandPos = new(g.position.X, expandYPosition);
+
+						int endPosY = (int)nextGalaxy.position.Y - (int)g.position.Y;
+						int expansionPosY = (int)nextGalaxy.position.Y - (int)expandPos.Y;
+						if (expansionPosY > 0 && expansionPosY < endPosY)
+						{
+							extraSteps++;
+						}
+					}
+
+					//Console.WriteLine("Next Galaxy: {0}, Steps without: {1}, Steps with: {2}", nextGalaxy.ID, stepsWithoutExpansion, stepsWithoutExpansion + extraSteps);
+					sumDistancesGalaxy += stepsWithoutExpansion + extraSteps;
 				}
 
 				galaxyIndex++;
