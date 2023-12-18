5ce736ece407e2cfcde40c7ee3a674ab1eb35385
Unain <vanstoel@gmail.com>
2023-12-18 16:01:44 +0100

    Refactored Day 16 Puzzle 1

--- Day16.cs
+++ Day16.cs
@@ -16,28 +16,19 @@ namespace AdventOfCode2023
 			}
 		}
 
-		public static void RunPuzzle1()
+		public static int GetEnergizedTiles(char[][] map, Vector2 startPos, Vector2 startDir)
 		{
-			string[] lines = File.ReadAllLines(@"Day16-Input.txt");
-			char[][] map = lines.Select(item => item.ToArray()).ToArray();
 			char[][] energizedMap = map.Select(a => a.ToArray()).ToArray();
 
 			char nextChar = '.';
-			Vector2 currentPos = new(0, 0);
-			Vector2 direction = new(1, 0);
+			Vector2 currentPos = startPos;
+			Vector2 direction = startDir;
 			List<Split> splits = new();
 			bool finished = false;
 			bool nextSplit = false;
 			int energizedTiles = 0;
-			energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-			if (map[(int)currentPos.Y][(int)currentPos.X] == '\\' || map[(int)currentPos.Y][(int)currentPos.X] == '|')
-			{
-				direction = new(0, 1);
-			}
-
 			while (!finished)
 			{
-				currentPos += direction;
 				if (currentPos.Y < 0 || currentPos.Y >= map.Length || currentPos.X < 0 || currentPos.X >= map[(int)currentPos.Y].Length || nextSplit)
 				{
 					nextSplit = false;
@@ -59,23 +50,16 @@ namespace AdventOfCode2023
 				}
 
 				nextChar = map[(int)currentPos.Y][(int)currentPos.X];
-				if (nextChar == '.')
-				{
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
 
 				if (nextChar == '\\')
 				{
 					direction = new(direction.Y, direction.X);
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
 				}
 				else if (nextChar == '/')
 				{
 					direction = new(-direction.Y, -direction.X);
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
 				}
-				else if (nextChar == '|' && direction.X == 1)
+				else if (nextChar == '|' || nextChar == '-')
 				{
 					if (energizedMap[(int)currentPos.Y][(int)currentPos.X] == '#')
 					{
@@ -83,85 +67,52 @@ namespace AdventOfCode2023
 						continue;
 					}
 
-					direction = new(0, 1);
-					splits.Add(new Split(currentPos, new(0, -1)));
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
-				else if (nextChar == '|' && direction.X == -1)
-				{
-					if (energizedMap[(int)currentPos.Y][(int)currentPos.X] == '#')
+					if (direction.X == 1 && nextChar == '|')
 					{
-						nextSplit = true;
-						continue;
+						direction = new(0, 1);  // Down + Split Up
+						splits.Add(new Split(currentPos, new(0, -1)));
 					}
-
-					direction = new(0, -1);  // Up + Split Down
-					splits.Add(new Split(currentPos, new(0, 1)));
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
-				else if (nextChar == '-' && direction.Y == 1)
-				{
-					if (energizedMap[(int)currentPos.Y][(int)currentPos.X] == '#')
+					else if (direction.X == -1 && nextChar == '|')
 					{
-						nextSplit = true;
-						continue;
+						direction = new(0, -1);  // Up + Split Down
+						splits.Add(new Split(currentPos, new(0, 1)));
 					}
-
-					direction = new(1, 0);  // Right + Split Left
-					splits.Add(new Split(currentPos, new(-1, 0)));
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
-				else if (nextChar == '-' && direction.Y == -1)
-				{
-					if (energizedMap[(int)currentPos.Y][(int)currentPos.X] == '#')
+					else if (direction.Y == 1 && nextChar == '-')
 					{
-						nextSplit = true;
-						continue;
+						direction = new(1, 0);  // Right + Split Left
+						splits.Add(new Split(currentPos, new(-1, 0)));
 					}
+					else if (direction.Y == -1 && nextChar == '-')
+					{
+						direction = new(-1, 0);  // Left + Split Right
+						splits.Add(new Split(currentPos, new(1, 0)));
+					}
+				}
 
-					direction = new(-1, 0);  // Left + Split Right
-					splits.Add(new Split(currentPos, new(1, 0)));
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
-				else if (nextChar == '|' && direction.Y != 0)
-				{
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
-				else if (nextChar == '-' && direction.X != 0)
-				{
-					energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
-					continue;
-				}
+				energizedMap[(int)currentPos.Y][(int)currentPos.X] = '#';
+				currentPos += direction;
 			}
-
-			for (int y = 0; y < map.Length; y++)
-			{
-				for (int x = 0; x < map[y].Length; x++)
-				{
-					Console.Write(map[y][x]);
-				}
-				Console.Write(Environment.NewLine);
-			}
-
-			Console.WriteLine("----------------------------");
 
 			for (int y = 0; y < energizedMap.Length; y++)
 			{
 				for (int x = 0; x < energizedMap[y].Length; x++)
 				{
-					Console.Write(energizedMap[y][x]);
 					if (energizedMap[y][x] == '#')
 					{
 						energizedTiles++;
 					}
 				}
-				Console.Write(Environment.NewLine);
 			}
+
+			return energizedTiles;
+		}
+
+		public static void RunPuzzle1()
+		{
+			string[] lines = File.ReadAllLines(@"Day16-Input.txt");
+			char[][] map = lines.Select(item => item.ToArray()).ToArray();
+
+			int energizedTiles = GetEnergizedTiles(map, new Vector2(0, 0), new Vector2(1, 0));
 
 			Console.WriteLine("Answer Puzzle 1: " + energizedTiles);
 		}
