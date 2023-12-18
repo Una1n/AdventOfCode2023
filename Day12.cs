720826a0869f081660bda3dc60ffa08df1b5872e
Unain <vanstoel@gmail.com>
2023-12-18 17:08:52 +0100

    Added Day 12 Puzzle 1 Start

--- Day12.cs
+++ Day12.cs
@@ -6,6 +6,17 @@
 		{
 			string[] lines = File.ReadAllLines(@"Day12-Input.txt");
 
+			foreach (string line in lines)
+			{
+				string[] splitLine = line.Split(' ');
+				string[] splitIndexes = splitLine[1].Split(',');
+
+				int countQuestionableSpots = splitLine[0].Count(c => c == '?');
+				for (int i = 1; i <= countQuestionableSpots; i++)
+				{
+					//
+				}
+			}
 
 			//Console.WriteLine("Answer Puzzle 1: " + sumHistory);
 		}
