3b2e2ec513c45195b6afc0465bc0d8af59c81768
Unain <vanstoel@gmail.com>
2023-12-18 11:18:37 +0100

    Added Day 15 Puzzle 1 Answer

--- Day15.cs
+++ Day15.cs
@@ -5,10 +5,24 @@
 		public static void RunPuzzle1()
 		{
 			string[] lines = File.ReadAllLines(@"Day15-Input.txt");
+			string sequence = lines[0];
+			int currentValue = 0;
+			int sumValues = 0;
+			for (int i = 0; i < sequence.Length; i++)
+			{
+				if (sequence[i] == ',')
+				{
+					sumValues += currentValue;
+					currentValue = 0;
+					continue;
+				}
 
+				currentValue += sequence[i];
+				currentValue *= 17;
+				currentValue %= 256;
+			}
 
-
-			//Console.WriteLine("Answer Puzzle 1: " + sumHistory);
+			Console.WriteLine("Answer Puzzle 1: " + (sumValues + currentValue));
 		}
 
 		public static void RunPuzzle2()
